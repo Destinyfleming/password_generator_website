056e6c6be8b9b1fb76a42fef2a40761ce2f23ae4
Destiny Fleming <destiny.fleming7401@gmail.com>
2020-11-20 12:22:47 -0800

    added a while loop and a for loop

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 
-/*var doYouUpper = confirm('Do you want upper case letters?')
-var doYouLower = confirm('Do you want lower case letters?')
-var doYouSpecial = confirm('Do you want special characters?')
-var doYouNumber = confirm ('Do you want numbers?')*/
+var userInput = document.getElementById('userInput').value;
 var passLength = prompt("How many characters would you like your password? Choose between 8 and 128")
 //var userInput = document.getElementById('userInput').value;
 var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N',"O",'P','Q','R','S','T','U','V','W','X','Y','Z']
+var randomUpp = upperCase[Math.floor(Math.random() * upperCase.length)];
 var lowerCase =['a','b','c','d','e','f','g','h','i','j','k','l','m','n',"o",'p','q','r','s','t','u','v','w','x','y','z']
-var numberArr = [0,1,2,3,4,5,6,7,8,9]
+var randomLow = lowerCase[Math.floor(Math.random() * lowerCase.length)];
+var numberArr = [0,1,2,3,4,5,6,7,8,9];
+var randomNum= numberArr[Math.floor(Math.random() * numberArr.length)];
 var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+var randomSpecial = specialChar[Math.floor(Math.random() * specialChar.length)];
 var generateBtn = document.querySelector("#generate"); document.getElementById('generateBtn')
 
 
@@ -27,10 +28,9 @@ function writePassword() {
   passwordText.value = password;
 };
 
-function generatePassword(passLength){
+function generatePassword(){}
 
-alert(passLength);
-}
+alert(passLength)
 
 if (passLength > 128) {
   alert("please put a value between 8 and 128.");
@@ -39,8 +39,15 @@ else if (passLength < 8) {
   alert("please put a value between 8 and 128.");
 }
 else {
-  var doYouUpper = confirm('Do you want upper case letters?')
-  var doYouLower = confirm('Do you want lower case letters?')
-  var doYouSpecial = confirm('Do you want special characters?')
-  var doYouNumber = confirm ('Do you want numbers?')
+  var doYouUpper = confirm('Do you want upper case letters?');
+  var doYouLower = confirm('Do you want lower case letters?');
+  var doYouSpecial = confirm('Do you want special characters?');
+  var doYouNumber = confirm ('Do you want numbers?');
+}
+while (doYoulower == false && doYouUpper == false && doYouNumber == false && doYouSpecial == false) {
+  alert("Please select one type of character");
+  doYouUpper = confirm('Do you want upper case letters?');
+  doYouLower = confirm('Do you want lower case letters?');
+  doYouSpecial = confirm('Do you want special characters?');
+  doYouNumber = confirm ('Do you want numbers?');
 }
