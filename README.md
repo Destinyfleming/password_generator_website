4e2f5fb8a768d3bd84786c033150f05d0a847db1
Destiny Fleming <destiny.fleming7401@gmail.com>
2020-11-23 06:27:41 -0800

    added to the read me and added some if, else statements in js.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # password_generator_website
+
+In this project I wrote the Javascript for a website to prompt a user to pick through a list of criteria and for the code to generate a random password based off the criteria.
+
+You can fin a screenshot of the website here : 
+<img
+src = "./assets/images/password-gen.png"
+raw=true
+alt=“Subject password generator screenshot”
+style=“margin-right: 10px;”
+/>
+
+I also linked the hosted webpage here:
+[a link] ( https://destinyfleming.github.io/password_generator_website/)
--- script.js
+++ script.js
@@ -1,42 +1,114 @@
 
-var newPass = ""
-var passLength = prompt("How many characters would you like your password? Choose between 8 and 128")
 var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N',"O",'P','Q','R','S','T','U','V','W','X','Y','Z']
-var randomUpp = upperCase[Math.floor(Math.random() * upperCase.length)];
 var lowerCase =['a','b','c','d','e','f','g','h','i','j','k','l','m','n',"o",'p','q','r','s','t','u','v','w','x','y','z']
-var randomLow = lowerCase[Math.floor(Math.random() * lowerCase.length)];
 var numberArr = [0,1,2,3,4,5,6,7,8,9];
-var randomNum= numberArr[Math.floor(Math.random() * numberArr.length)];
 var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
-var randomSpecial = specialChar[Math.floor(Math.random() * specialChar.length)];
 var generateBtn = document.querySelector("#generate"); document.getElementById('generateBtn')
+var confirmLower;
+var confirmUpper;
+var confirmNumber;
+var confirmSpecial;
+var userChoices;
+var passLength;
 
-
+generateBtn.addEventListener("click", writePassword);
 
 function generatePassword(){
 
-alert(passLength)
+  passLength = prompt ('How long do you want your password? Choose between 8-128.')
 
-if (passLength > 128) {
-  alert("please put a value between 8 and 128.");
-}
-else  (passLength < 8) {
-  alert("please put a value between 8 and 128.");
-}
-else {
-  var doYouUpper = confirm('Do you want upper case letters?');
-  var doYouLower = confirm('Do you want lower case letters?');
-  var doYouSpecial = confirm('Do you want special characters?');
-  var doYouNumber = confirm ('Do you want numbers?');
+  if (passLength > 128) {
+    alert("please put a value between 8 and 128.");
+  }
+  else if (passLength < 8) {
+    alert("please put a value between 8 and 128.");
+  }
+  else {
+    confirmUpper= confirm('Do you want upper case letters?');
+    confirmLower = confirm('Do you want lower case letters?');
+    confirmSpecial = confirm('Do you want special characters?');
+    confirmNumber= confirm ('Do you want numbers?');
+  };
+
+  if (!confirmLower && !confirmUpper && !confirmNumber && !confirmSpecial) {
+    userChoices = alert("You must choose a criteria");
+  } else if (confirmLower && confirmUpper && confirmNumber && confirmSpecial) {
+    userChoices = lowerCase.concat(upperCase, numberArr, specialChar);
+    ;
+  }
+
+  else if (confirmLower && confirmUpper && confirmNumber) {
+    userChoices = lowerCase.concat(upperCase, numbersArr);
+    console.log(userChoices);
+  }
+  else if (confirmLower && confirmUpper && confirmSpecial) {
+    userChoices = lowerCase.concat(upperCase, specialChar);
+    console.log(userChoices);
+  }
+  else if (confirmLower && confirmNumber && confirmSpecial) {
+    userChoices = lowerCase.concat(numbersArr, specialChar);
+    console.log(userChoices);
+  }
+  else if (confirmUpper && confirmNumber && confirmSpecial) {
+    userChoices = upperCase.concat(numbersArr, specialChar);
+    console.log(userChoices);
+  }
+
+  else if (confirmLower && confirmUpper) {
+    userChoices = lowerCase.concat(upperCase);
+    console.log(userChoices);
+  }
+  else if (confirmLower && confirmNumber) {
+    userChoices = lowerCase.concat(numbersArr);
+    console.log(userChoices);
+  }
+  else if (confirmLower && confirmSpecial) {
+    userChoices = lowerCase.concat(specialChar);
+    console.log(userChoices);
+  }
+  else if (confirmUpper && confirmNumber) {
+    userChoices = upperCase.concat(numbersArr);
+    console.log(userChoices);
+  }
+  else if (confirmUpper && confirmSpecial) {
+    userChoices = upperCase.concat(specialChar);
+    console.log(userChoices);
+  }
+  else if (confirmNumber && confirmSpecial) {
+    userChoices = numbers.concat(specialChar);
+    console.log(userChoices);
+  }
+
+  else if (confirmLower) {
+    userChoices = lowerCase;
+    console.log(userChoices);
+  }
+  else if (confirmUpper) {
+    userChoices = blankUpper.concat(upperCase);
+    console.log(userChoices);
+  }
+  else if (confirmNumber) {
+    userChoices = numbersArr;
+    console.log(userChoices);
+  }
+  else if (confirmSpecial) {
+    userChoices = specialChar;
+    console.log(userChoices);
+  };
 }
 
-while (doYoulower == false && doYouUpper == false && doYouNumber == false && doYouSpecial == false) {
-  alert("Please select one type of character");
-  doYouUpper = confirm('Do you want upper case letters?');
-  doYouLower = confirm('Do you want lower case letters?');
-  doYouSpecial = confirm('Do you want special characters?');
-  doYouNumber = confirm ('Do you want numbers?');
-}
+
+var passwordBlank = [];
+  
+for (var i = 0; i < passLength; i++) {
+  var allChoices = userChoices[Math.floor(Math.random() * userChoices.length)];
+  passwordBlank.push(allChoices);
+  console.log(allChoices);
+
+  var password = passwordBlank.join("");
+  alert("Your Pasword is: " + password);
+  return password;
+  
 }
 
 function writePassword() {
