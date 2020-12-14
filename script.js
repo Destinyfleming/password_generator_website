d41bc827797c8c141e19da19f275e6cc8952cab1
Destiny Fleming <destiny.fleming7401@gmail.com>
2020-12-13 18:41:51 -0800

    fixed a spelling error

--- script.js
+++ script.js
@@ -9,28 +9,22 @@ var uppercaseArr =['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','
 // prompt user for password values
 
 var passwordLength = prompt('How long would you like your password?')
-if (length < 8) {
+
+if (passwordLength < 8) {
   alert('Your password has to be at least 8 characters long.');
   return;
-}else (length > 128) {
+}else (passwordLength > 128) {
   alert('Your password has to have less then 128 characters.');
   return;
 }
+
 var confirmSpecial = confirm('Would you like your password to contain special characters?')
 var confirmNumber = confirm('Would you like your password to contain numbers?')
 var confirmLower = confirm('Would you like your password to contain lowercase letters?')
 var confirmUpper = confirm('Would you like your password to contain uppercase letters?')
 
 // tells what user entered
-var passwordCritera = {
-  length: length,
-  hasSpecialCharacters: hasSpecialCharacters,
-  hasNumericCharacters: hasNumericCharacters,
-  hasLowerCasedCharacters: hasLowerCasedCharacters,
-  hasUpperCasedCharacters: hasUpperCasedCharacters
-};
 
-return passwordCritera;
 //get random things from corresponding arrays based on users answers to prompts
 
 // Write password to the #password input
