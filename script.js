57a50c39fb35650c89e4c8078cfd2d014e303300
Destiny Fleming <destiny.fleming7401@gmail.com>
2021-03-07 14:52:31 -0800

    updated a few if, else sections

--- script.js
+++ script.js
@@ -1,49 +1,59 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+var numArr = [1,2,3,4,5,6,7,8,9,0]
 var letterArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 var charArr = ['`','!','@','@','#','$','<','.','>','/','?','*','^','&','%']
+var userPass =''
 
-/*function passwordLength(){
-  var length = prompt('How long do you want your password? Should be between 8 and 128 characters.');
-if(length <8){
-  alert('please choose a higher number.')
-}else if(length >128){
-  alert('please choose a lower number.')
-}else{
-  generatePassword()
-}}
-*/
+
 function generatePassword(){
-  var length = prompt('How long do you want your password? Should be between 8 and 128 characters.');
-  if(length <8){
+
+  var passLength = prompt('How long do you want your password? Should be between 8 and 128 characters.');
+  
+  if(passLength <8){
     alert('please choose a higher number.')
-  }else if(length >128){
+  }else if(passLength >128){
     alert('please choose a lower number.')
   }else{
+
     var DoYouNum = confirm('Do you want numbers?');
     var DoYouLett = confirm('Do you want letters?');
     var DoYouChar = confirm('Do you want special characters?');
+
     if(DoYouNum === true && DoYouChar === true && DoYouLett === true){
     //random generate from each arr
+    console.log(userPass)
+    userPass = charArr.concat(letterArr, numArr) 
     }else if(DoYouNum === false && DoYouChar === true && DoYouLett === true){
       //random generate char + lett
+      userPass = charArr.concat(letterArr);
     }else if (DoYouNum === true && DoYouChar === false && DoYouLett === true){
       //random generate num + let
+      userPass = numArr.concat(letterArr);
     }else if (DoYouNum === true && DoYouChar === true && DoYouLett === false){
       //random generate num + char
+      userPass = numArr.concat(charArr);
     }else if (DoYouNum === false && DoYouChar === false && DoYouLett === true){
       //random generate let
+      userPass = letterArr
     }else if (DoYouNum === false && DoYouChar === true && DoYouLett === false){
-      //random generate char
+      //random generate char 
+      userPass = charArr
     }else if (DoYouNum === true && DoYouChar === false && DoYouLett === false){
-      //random generate num 
-    }else{
+       //random generate num
+       for (var i = 0 ; i === passLength.length; i++)
+      {
+        Math.floor(Math.random() * 10);
+      } 
+    }else {
       alert('please click generate password button again and pick at least one field.')
     }
-  }
-}
+    return userPass
+}}
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
